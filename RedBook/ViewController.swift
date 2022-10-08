4ba10279276372b1909f3d021cdc2b46b59dbff7
yuximin <whaledestine@163.com>
2022-10-08 21:22:20 +0800

    new: 新建工程

---
+++ RedBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RedBook
+//
+//  Created by apple on 2022/10/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
